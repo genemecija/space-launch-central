3bb402dfede9f5f29acb045f23ae10faaa307b68
Gene Mecija <genemecija@outlook.com>
2020-04-10 00:20:25 -0700

    Started timing functionality

--- src/components/Vehicle.js
+++ src/components/Vehicle.js
@@ -3,15 +3,41 @@ import moment from "moment"
 
 export default function Vehicle(props) {
 
-    // const time = moment(props.launchDate).subtract("now")
-    // console.log(time);
+    function startClocks() {
+        let now = new Date().getTime()
+        // let launch = new Date('2020-03-09 20:00:00 PST').getTime();
+        // let launch = new Date(2020, 3, 9, 22, 20, 0, 0).getTime()
+        // var strDateTime = "Fri, 18 Oct 2013 11:38:23 GMT";
+        let launch = new Date('Apr 09 2020 23:56:00 GMT-700')
+        // let launch = new Date('2020-04-15T00:00:00Z')
+        console.log(launch.toLocaleString());
 
-    const clock = setInterval(() => {
-        var today = new Date();
-        var time = today.getFullYear() + "-" + today.getMonth() + "-" + today.getDate() + ":" + today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-        console.log(time);
-        // get time remaining between now and launchDate
-    }, [1000]);
+        const timeUntil = launch - now;
+        let duration = moment.duration(timeUntil, 'milliseconds');
+        
+        let interval
+        if (duration.days() > 1) { // If greater than 1 day, iterate per day
+            interval = 1000*60*60*24
+        } else if (duration.hours() > 3) { // If greater than 6 hours, iterate per hours
+            interval = 1000*60*60
+        } else if (duration.hours() > 1) { // If greater than 1 hour, iterate per minute
+            interval = 1000*60
+        } else { // If less than 1 hour, iterate per second
+            interval = 1000
+        }
+        const clock = setInterval(() => {
+            duration = moment.duration(duration - 1000, 'milliseconds');
+            console.log(duration.days() + " days " + duration.hours() + ":" + duration.minutes() + ":" + duration.seconds())
+            // get time remaining between now and launchDate
+        }, [interval]);
+        return clock
+    }
+
+    startClocks()
+    // const clockChecker = setInterval(() => {
+    //     console.log('clockChecker');
+    //     startClocks()
+    // }, [5000])
     
 
     return (
